efcea79f1f02e7c38702f70ece20a3423c68d012
rodyherrera <contact@rodyherrera.com>
2026-06-16 02:05:02 -0400

    chore: update version to 2.0.1 in conanfile.py, plugin.json, and vpm.json; enhance dependencies

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 class CommonNeighborAnalysisConan(ConanFile):
     name = "common-neighbor-analysis"
-    version = "2.0.0"
+    version = "2.0.1"
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
@@ -12,8 +12,8 @@ class CommonNeighborAnalysisConan(ConanFile):
     requires = (
         "boost/1.88.0",
         "onetbb/2021.12.0",
-        "coretoolkit/[>=2.0]",
-        "structure-identification/[>=2.0]",
+        "coretoolkit/[>=2.5]",
+        "structure-identification/[>=2.1]",
         "spdlog/1.14.1",
         "nlohmann_json/3.11.3",
         "yaml-cpp/0.8.0",
