3d1da44bd7ed27420600f28c0d5578b1ea93013d
rodyherrera <contact@rodyherrera.com>
2026-08-16 01:45:12 -0400

    refactor: derive the conan version from vpm.json

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,12 @@
+import json
+import os
+
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 
 class CommonNeighborAnalysisConan(ConanFile):
     name = "common-neighbor-analysis"
-    version = "2.0.4"
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
@@ -18,7 +20,13 @@ class CommonNeighborAnalysisConan(ConanFile):
         "nlohmann_json/3.11.3",
         "yaml-cpp/0.8.0",
     )
+    exports = "vpm.json"
     exports_sources = "CMakeLists.txt", "include/*", "src/*"
+
+    def set_version(self):
+        manifest = os.path.join(self.recipe_folder, "vpm.json")
+        with open(manifest, encoding="utf-8") as stream:
+            self.version = json.load(stream)["version"]
 
     def layout(self):
         cmake_layout(self)
